38e50b6e4b6633e33230368fa27c263082102280
Sai Sridhar <sridhar.akula001@gmail.com>
2024-04-02 13:11:39 +0530

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,22 @@ export const metadata: Metadata = {
 	title: "Sai Sridhar's Portfolio",
 	description:
 		"A portfolio of Akula Sai Sridhar showcasing his skills and experience as a Fullstack and an App Developer. Learn more about his work and experience.",
+	openGraph: {
+		type: "website",
+		url: "https://saisridhar.vercel.app",
+		countryName: "India",
+		title: "Portfolio of Sai Sridhar",
+		description:
+			"A portfolio of Akula Sai Sridhar showcasing his skills and experience as a Fullstack and an App Developer. Learn more about his work and experience.",
+		siteName: "Sai Sridhar's Portfolio",
+	},
+	twitter: {
+		title: "Portfolio of Sai Sridhar",
+		description:
+			"A portfolio of Akula Sai Sridhar showcasing his skills and experience as a Fullstack and an App Developer. Learn more about his work and experience.",
+		site: "https://saisridhar.vercel.app",
+		card: "summary_large_image",
+	},
 };
 
 export default function RootLayout({
